8b3feeea2f9f43a5d6b2897d35481956cc687a17
William Dines <williamdines@rocketmail.com>
2024-05-09 12:43:15 -0500

    adjust json file

--- encounters/Index.py
+++ encounters/Index.py
@@ -3,5 +3,5 @@ import json
 class Encounters:
   def __init__(self):
     with open('./encounters/encounterTypes.JSON', 'r') as file:
-      self.encounterTypes = json.load(file)['encounters']
+      self.encounterTypes = json.load(file)
       file.close()
